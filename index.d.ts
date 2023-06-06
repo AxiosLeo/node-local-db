6a4c1bb547c0104d64b1400b08aabf840e1fbf60
axiosleo <axioscros@aliyun.com>
2023-06-06 13:33:39 +0800

    type: statements

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,14 @@
 export declare class QueryTable {
   constructor(datapath: string);
 
+  /**
+   * count data
+   */
   count(): Promise<number>;
 
+  /**
+   * find data by uniq
+   */
   find<T extends Object>(uniq: string): Promise<T | undefined>;
 
   /**
@@ -12,21 +18,48 @@ export declare class QueryTable {
    */
   select<T extends Object>(offset?: number, limit?: number): Promise<T[]>;
 
+  /**
+   * check data is exist
+   */
   exist(uniq: string): Promise<boolean>;
 
+  /**
+   * insert or update data
+   */
   upsert<T extends Object>(uniq: string, data: T): Promise<boolean>;
 
+  /**
+   * upsert many data
+   * return failed row
+   */
+  upsertMany<T extends Object>(data: T[]): Promise<T[]>;
+
+  /**
+   * insert data only if uniq is not exist
+   */
   insert<T extends Object>(uniq, data: T): Promise<boolean>;
 
+  /**
+   * update data only if uniq is exist
+   */
   update<T extends Object>(uniq, data: T): Promise<boolean>;
 
+  /**
+   * delete data by uniq
+   */
   delete(uniq: string): Promise<boolean>;
 }
 
 export declare class LocalDB {
   constructor(root: string);
 
+  /**
+   * get table list
+   */
   tables(prefix?: string): Promise<string[]>;
 
-  table(table_name: string): Promise<QueryTable>;
+  /**
+   * get table instance
+   */
+  table(table_name: string): QueryTable;
 }
